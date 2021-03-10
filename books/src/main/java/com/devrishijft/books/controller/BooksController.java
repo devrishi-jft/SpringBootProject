5c7e92359011e772b28df32cf256f27a96fc17c9
devrishi-jft <devrishi.sherawat@jellyfishtechnologies.com>
2021-03-10 13:57:22 +0530

    CRUD operation complete

--- books/src/main/java/com/devrishijft/books/controller/BooksController.java
+++ books/src/main/java/com/devrishijft/books/controller/BooksController.java
@@ -3,9 +3,7 @@ package com.devrishijft.books.controller;
 import com.devrishijft.books.model.Books;
 import com.devrishijft.books.services.BooksServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,12 +12,37 @@ import java.util.List;
 public class BooksController {
     @Autowired
     private BooksServices booksServices;
+    //get all book handler
     @GetMapping("/books")
     public List<Books> getBooks(){
         return this.booksServices.getAllBooks();
     }
+
+    //get single book handler
     @GetMapping("/books/{id}")
     public Books getBook(@PathVariable("id") int id){
         return booksServices.getBookById(id);
     }
+
+    //new book handler
+    @PostMapping("/books")
+    public Books addBook(@RequestBody Books books){
+       Books b=this.booksServices.addBook(books);
+        return b;
+    }
+
+    //delete book handler
+    @DeleteMapping("/books/delete/{bookId}")
+    public void deletebook(@PathVariable("bookId") int bookId){
+        this.booksServices.deleteBook(bookId);
+        System.out.println("bookId = " + bookId);
+    }
+
+    //update book handler
+    @PutMapping("/books/update/{bookId}")
+    public Books updatebook(@RequestBody Books books,@PathVariable("bookId") int bookId){
+        this.booksServices.updateBook(books,bookId);
+        return books;
+    }
+
 }
--- books/src/main/java/com/devrishijft/books/services/BooksServices.java
+++ books/src/main/java/com/devrishijft/books/services/BooksServices.java
@@ -3,8 +3,11 @@ package com.devrishijft.books.services;
 import com.devrishijft.books.model.Books;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class BooksServices {
     private static List<Books> list= new ArrayList<>();
@@ -22,5 +25,29 @@ public class BooksServices {
         Books books=null;
         books=list.stream().filter(e->e.getId()==id).findFirst().get();
         return books;
+    }
+    //adding the book
+    public Books addBook(Books b){
+        list.add(b);
+        return b;
+    }
+
+    //delete book
+    public void deleteBook(int bookId) {
+        System.out.println("bookId 2 = " + bookId);
+       list= list.stream().filter(book->book.getId()!=bookId).
+               collect(Collectors.toList());
+        System.out.println("bookId 3= " + bookId);
+    }
+    //Update the book
+    public void updateBook(Books books, int bookId) {
+    list.stream().map(b->{
+        if(b.getId()==bookId){
+            b.setTitle(books.getTitle());
+            b.setAuthor(books.getAuthor());
+
+        }
+        return b;
+    }).collect(Collectors.toList());
     }
 }
